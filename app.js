077a3e1dc6748a434c2a81169e5981c9bd3a7fab
vitorsei <seiji.vitor@gmail.com>
2016-11-02 13:50:16 -0200

    Authorization with Passport

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
 
 var app = express();
 
@@ -18,8 +21,13 @@ var authRouter = require('./src/routes/authRoutes')(nav);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'library'}));
+
+require('./src/config/passport')(app);
 
 app.set('views', './src/views');
+
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
